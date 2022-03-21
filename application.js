3818413ce932831cb46b7b05f8474d30428ad553
dennis <dennisfelixps@gmail.com>
2022-03-21 14:23:21 -0400

    my first commit

---
+++ application.js
@@ -0,0 +1,38 @@
+console.log("Helloo Worlds");
+
+var FName = "Dennis";
+var LName = "Felix";
+
+console.log(FName+" "+LName);
+
+var y = 55;
+var l = 88;
+
+const person = {
+  firstName: "John",
+  lastName : "Doe",
+  id     :  5566
+};
+
+// Display some data from the object:
+document.getElementById("demo2").innerHTML =
+person.firstName + " " + person.lastName;
+
+
+
+function myButton(){
+   var NameChange = "Dennis Oketch2";
+   return NameChange;
+}
+
+
+function myFunction(p1, p2) {
+    return p1 * p2;  
+  }
+
+
+
+
+x = (y/11)+(l/11);
+document.getElementById("pr").innerHTML = x;
+document.getElementById("math").innerHTML = myFunction(88,11);
